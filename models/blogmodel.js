9a17a38796468229e6779d765ed3f173e5931502
vijay <vijayg954@gmail.com>
2024-08-27 21:11:42 +0530

    first commit

--- models/blogmodel.js
+++ models/blogmodel.js
@@ -1,10 +1,29 @@
+// import mongoose from "mongoose";
+
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   category:String,
+//   description: String,
+//     timestamps: true,
+
+// });
+
+// const Blog = mongoose.model("Blog", blogSchema);
+// export default Blog;
+
+
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  category:String,
-  description: String
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: String,
+    category: String,
+    description: String,
+  },
+  {
+    timestamps: true, // Correctly placed within the schema options
+  }
+);
 
 const Blog = mongoose.model("Blog", blogSchema);
 export default Blog;
